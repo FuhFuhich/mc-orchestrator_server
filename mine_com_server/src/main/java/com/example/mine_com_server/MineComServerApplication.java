d2cac9c840de6c6894d33c78dbe97a75712de9a6
FuhFuh <ilyegorow@yandex.ru>
2026-03-14 19:23:35 +0300

    upd

---
+++ mine_com_server/src/main/java/com/example/mine_com_server/MineComServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mine_com_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MineComServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MineComServerApplication.class, args);
+    }
+
+}
---
+++ mine_com_server/src/test/java/com/example/mine_com_server/MineComServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mine_com_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MineComServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
